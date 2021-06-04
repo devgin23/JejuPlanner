99b4e7565c59881abdfc8dac9b98a3cc141840c4
Jhi97 <jeon9771@naver.com>
2021-06-04 10:14:20 +0900

    Schedule[Controller, service, domain] 이름변경 => Board, 계획 일정 조회 Plan java단으로 이동

---
+++ JejuPlanner/src/main/java/com/gteam/planner/service/BoardService.java
@@ -0,0 +1,10 @@
+package com.gteam.planner.service;
+
+
+public interface BoardService {
+	
+	
+	// 일정 객체
+	/*public ScheduleVO formData() throws Exception;*/
+
+}
---
+++ JejuPlanner/src/main/java/com/gteam/planner/service/BoardServiceImpl.java
@@ -0,0 +1,19 @@
+package com.gteam.planner.service;
+
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class BoardServiceImpl implements BoardService{
+	
+	
+
+	
+	//일정 객체 출력
+/*	@Override
+	public ScheduleVO formData() throws Exception {
+		return dao.list().get(0);
+	}*/
+
+}
