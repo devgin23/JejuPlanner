044154dd12278bdf60d5d6ea281afa0dd6e1486c
Jhi97 <jeon9771@naver.com>
2021-06-08 11:48:44 +0900

    register.js 쓸모없는 행 지우기 & 회원정보 닉네임 삭제

--- JejuPlanner/src/main/webapp/resources/js/register.js
+++ JejuPlanner/src/main/webapp/resources/js/register.js
@@ -28,10 +28,7 @@ function fn_idCheck(){
 			}
 		});
 	}
-	
 }
-
-
 
 $(function(){
     //모달을 전역변수로 선언
@@ -61,77 +58,6 @@ $(function(){
         }
     });
     
-    //------- 검사하여 상태를 class에 적용
-    $('#userId').keyup(function(event){
-        
-        var divId = $('#divId');
-        
-        if($('#userId').val()==""){
-            divId.removeClass("has-success");
-            divId.addClass("has-error");
-        }else{
-            divId.removeClass("has-error");
-            divId.addClass("has-success");
-        }
-    });
-    
-    $('#userPw').keyup(function(event){
-        
-        var divPassword = $('#divPassword');
-        
-        if($('#userPw').val()==""){
-            divPassword.removeClass("has-success");
-            divPassword.addClass("has-error");
-        }else{
-            divPassword.removeClass("has-error");
-            divPassword.addClass("has-success");
-        }
-    });
-    
-    $('#userPwCheck').keyup(function(event){
-        
-        var passwordCheck = $('#userPwCheck').val();
-        var password = $('#userPw').val();
-        var divPasswordCheck = $('#divPasswordCheck');
-        
-        if((passwordCheck=="") || (password!=passwordCheck)){
-            divPasswordCheck.removeClass("has-success");
-            divPasswordCheck.addClass("has-error");
-        }else{
-            divPasswordCheck.removeClass("has-error");
-            divPasswordCheck.addClass("has-success");
-        }
-    });
-    
-    
-    $('#userNickname').keyup(function(event){
-        
-        var divNickname = $('#divNickname');
-        
-        if($.trim($('#userNickname').val())==""){
-            divNickname.removeClass("has-success");
-            divNickname.addClass("has-error");
-        }else{
-            divNickname.removeClass("has-error");
-            divNickname.addClass("has-success");
-        }
-    });
-    
-    $('#userMail').keyup(function(event){
-        
-        var divMail = $('#divMail');
-        
-        if($.trim($('#userMail').val())==""){
-            divMail.removeClass("has-success");
-            divMail.addClass("has-error");
-        }else{
-            divMail.removeClass("has-error");
-            divMail.addClass("has-success");
-        }
-    });
-
-    
-    
     //------- validation 검사
     $( "form" ).submit(function( event ) {
         
@@ -140,22 +66,15 @@ $(function(){
         var divPassword = $('#divPassword');
         var divPasswordCheck = $('#divPasswordCheck');
         var divName = $('#divName');
-        var divNickname = $('#divNickname');
         var divMail = $('#divMail');
-        
         
         //아이디 검사
         if($('#userId').val()==""){
             modalContents.text("아이디를 입력하여 주시기 바랍니다.");
             modal.modal('show');
             
-            divId.removeClass("has-success");
-            divId.addClass("has-error");
             $('#userId').focus();
             return false;
-        }else{
-            divId.removeClass("has-error");
-            divId.addClass("has-success");
         }
         //아이디 중복 검사
         var idCheckVal = $("#idCheck").val();
@@ -176,25 +95,14 @@ $(function(){
     		modal.modal('show');
     		return false;
     	}
-    	//아이디 중복검사 통과 시
-    	else if(idCheckVal == "Y"){
-    		divId.removeClass("has-error");
-            divId.addClass("has-success");
-    	}
-        
         
         //패스워드 검사
         if($('#userPw').val()==""){
             modalContents.text("패스워드를 입력하여 주시기 바랍니다.");
             modal.modal('show');
             
-            divPassword.removeClass("has-success");
-            divPassword.addClass("has-error");
             $('#userPw').focus();
             return false;
-        }else{
-            divPassword.removeClass("has-error");
-            divPassword.addClass("has-success");
         }
         
         //패스워드 확인
@@ -202,13 +110,8 @@ $(function(){
             modalContents.text("패스워드 확인을 입력하여 주시기 바랍니다.");
             modal.modal('show');
             
-            divPasswordCheck.removeClass("has-success");
-            divPasswordCheck.addClass("has-error");
             $('#userPwCheck').focus();
             return false;
-        }else{
-            divPasswordCheck.removeClass("has-error");
-            divPasswordCheck.addClass("has-success");
         }
         
         //패스워드 비교
@@ -216,27 +119,8 @@ $(function(){
             modalContents.text("패스워드가 일치하지 않습니다.");
             modal.modal('show');
             
-            divPasswordCheck.removeClass("has-success");
-            divPasswordCheck.addClass("has-error");
             $('#userPwCheck').focus();
             return false;
-        }else{
-            divPasswordCheck.removeClass("has-error");
-            divPasswordCheck.addClass("has-success");
-        }  
-        
-        //별명
-        if($('#userNickname').val()==""){
-            modalContents.text("별명을 입력하여 주시기 바랍니다.");
-            modal.modal('show');
-            
-            divNickname.removeClass("has-success");
-            divNickname.addClass("has-error");
-            $('#userNickname').focus();
-            return false;
-        }else{
-            divNickname.removeClass("has-error");
-            divNickname.addClass("has-success");
         }
         
         //이메일
@@ -244,17 +128,8 @@ $(function(){
             modalContents.text("이메일을 입력하여 주시기 바랍니다.");
             modal.modal('show');
             
-            divMail.removeClass("has-success");
-            divMail.addClass("has-error");
             $('#userMail').focus();
             return false;
-        }else{
-            divMail.removeClass("has-error");
-            divMail.addClass("has-success");
         }
-        
-
-    
     });
-    
 });
